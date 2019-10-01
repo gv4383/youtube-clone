e93cc316a5135c8759b8d80fe032c4228de2faed
Greg Vargas <greg.vargas@silvercar.com>
2019-09-30 19:25:28 -0500

    Added image loading and caching

--- app/src/main/java/com/skwerlhub/youtubeclone/MainAdapter.kt
+++ app/src/main/java/com/skwerlhub/youtubeclone/MainAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import androidx.recyclerview.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.video_row.view.*
 
 class MainAdapter(val homeFeed: HomeFeed): RecyclerView.Adapter<CustomViewHolder>() {
@@ -24,6 +25,13 @@ class MainAdapter(val homeFeed: HomeFeed): RecyclerView.Adapter<CustomViewHolder
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
         val video = homeFeed.videos.get(position)
         holder.view.textView_video_title.text = video.name
+        holder.view.textView_channel_name.text = video.channel.name + " • " + "20K Views\n4 days ago"
+
+        val thumbnailImageView = holder.view.imageView_video_thumbnail
+        Picasso.get().load(video.imageUrl).into(thumbnailImageView)
+
+        val channelProfileImageView = holder.view.imageView_channel_profile
+        Picasso.get().load(video.channel.profileImageUrl).into(channelProfileImageView)
     }
 }
 
